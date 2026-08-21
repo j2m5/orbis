86d40269c13871780eb63c2767ed3d5703639009
dmitry <roffits@gmail.com>
2026-08-21 16:14:12 +0300

    event emitter

--- src/core/framework/events/EventEmitter.ts
+++ src/core/framework/events/EventEmitter.ts
@@ -1,11 +1,11 @@
 type EventMap = Record<string, unknown>
 
 interface Observer<M extends EventMap, K extends keyof M & string = keyof M & string> {
-  update(event: K, emitter: object, data?: M[K]): void
+  update(event: K, data?: M[K]): void
 }
 
 class EventEmitter<M extends EventMap> {
-  private readonly observers: Map<string, Observer<M, any>[]> = new Map()
+  private readonly observers: Map<string, Observer<M>[]> = new Map()
 
   public constructor() {
     this.observers.set('*', [])
@@ -13,7 +13,7 @@ class EventEmitter<M extends EventMap> {
 
   public attach<K extends keyof M & string>(observer: Observer<M, K>, event: K): void
   public attach(observer: Observer<M>, event?: '*'): void
-  public attach(observer: Observer<M, any>, event: string = '*'): void {
+  public attach(observer: Observer<M>, event: string = '*'): void {
     const group = this.observers.get(event) || []
 
     group.push(observer)
@@ -21,7 +21,7 @@ class EventEmitter<M extends EventMap> {
     this.observers.set(event, group)
   }
 
-  public detach(observer: Observer<M, any>, event: (keyof M & string) | '*'): void {
+  public detach(observer: Observer<M>, event: (keyof M & string) | '*'): void {
     const group = this.observers.get(event)
 
     if (!group) return
@@ -29,12 +29,12 @@ class EventEmitter<M extends EventMap> {
     this.observers.set(event, group.filter(o => o !== observer))
   }
 
-  public emit<K extends keyof M & string>(event: K, emitter: object, data?: M[K]): void {
+  public emit<K extends keyof M & string>(event: K, data?: M[K]): void {
     const group = this.observers.get(event) || []
     const all = this.observers.get('*') || []
 
     for (const observer of [...group, ...all]) {
-      observer.update(event, emitter, data)
+      observer.update(event, data)
     }
   }
 }
